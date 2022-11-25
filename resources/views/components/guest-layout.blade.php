bcaaada6ade3e16d49ba2d50ad83a6cd4e443a6a
Akmal Maulana Basri <editorzone.20@gmail.com>
2022-11-25 12:31:05 +0700

    asset()

--- resources/views/components/guest-layout.blade.php
+++ resources/views/components/guest-layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>    
-    <link rel="stylesheet" href="assets/app.e4a36eae.css">
+    <link rel="stylesheet" href="{{ asset('assets/app.e4a36eae.css') }}">
 
 </head>
 
